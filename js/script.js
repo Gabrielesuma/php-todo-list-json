69b87971ad37e931e3b8f70c8eed8c0f03563c23
Gabrielesuma <gabrielesuma139@gmail.com>
2024-05-06 17:17:37 +0200

    get contents

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            todo: [],
+            itemText: '',
+            apiUrl: 'server.php'
+        }
+    },
+    methods:{
+        getData(){
+            axios.get(this.apiUrl).then((res)=>{
+                this.todo = res.data;
+            })
+        },
+        removeItem(id){
+            const i = this.todo.findIndex((el)=> el.id === id);
+            if(i!== -1){
+                this.todo.splice(i, 1);
+            }
+        },
+        addTodo(){
+            const newObj = {
+                id: null,
+                text: this.itemText,
+                done: false
+            }
+            let nextId = 0;
+            this.todo.forEach((el) => {
+                if(nextId < el.id){
+                    nextId = el.id;
+                }
+            });
+            newObj.id = nextId + 1;
+            this.todo.push(newObj);
+            this.itemText = '';
+            //console.log(this.todo);
+        },
+        toogleItem(id){
+            const item = this.todo.find((el) => {
+                return el.id === id;
+            })
+            if(item){
+                item.done = !item.done;
+            }
+        }
+    },
+    mounted(){
+        //console.log(this.todo);
+        this.getData();
+    }
+}).mount('#app');
---
+++ server.php
@@ -0,0 +1,7 @@
+<?php
+
+$todoJson = file_get_contents("js/data.json");
+//var_dump($todoJson);
+
+header("Content-Type: application/json");
+echo $todoJson;
