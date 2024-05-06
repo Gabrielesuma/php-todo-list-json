cdc2fc769ce15961f02df0451cb366a260360fda
Gabrielesuma <gabrielesuma139@gmail.com>
2024-05-06 18:36:12 +0200

    update

--- js/script.js
+++ js/script.js
@@ -5,13 +5,19 @@ createApp({
         return{
             todo: [],
             itemText: '',
-            apiUrl: 'server.php'
+            apiUrl: 'server.php',
+            lastId: null,
+            newItem: {
+                text: "",
+                done: ""
+            }
         }
     },
     methods:{
         getData(){
             axios.get(this.apiUrl).then((res)=>{
                 this.todo = res.data;
+                this.lastId = this.data.length - 1;
             })
         },
         removeItem(id){
@@ -21,7 +27,15 @@ createApp({
             }
         },
         addTodo(){
-            const newObj = {
+            const items = {...newItem};
+            this.newItem = {
+                text: "",
+                done: ""
+            };
+            this.lastId+= 1;
+            items.id = this.lastId;
+            this.data.push(items);
+            /*const newObj = {
                 id: null,
                 text: this.itemText,
                 done: false
@@ -35,6 +49,14 @@ createApp({
             newObj.id = nextId + 1;
             this.todo.push(newObj);
             this.itemText = '';
+            const data = new FormData();
+            for(let key in newObj){
+                data.append(key, newObj[key]);
+            }*/
+            axios.post(this.apiUrl, data).then((res)=>{
+                console.log(res.data);
+                this.lastId = this.data.length - 1;
+            })
             //console.log(this.todo);
         },
         toogleItem(id){
--- server.php
+++ server.php
@@ -3,5 +3,19 @@
 $todoJson = file_get_contents("js/data.json");
 //var_dump($todoJson);
 
+if(isset($_POST['id'])){
+    var_dump($_POST);
+    $todo = json_decode($todoJson, true);
+    $todoItem = [
+        'id'=> (int)$_POST['id'],
+        'text'=> $_POST['text'],
+        'done'=> $_POST['done'],
+        
+    ];
+    $todo[] = $todoItem;
+    $todoJson = json_encode($todo, JSON_PRETTY_PRINT);
+    file_put_contents('data.json', $todoJson);
+}
+
 header("Content-Type: application/json");
 echo $todoJson;
